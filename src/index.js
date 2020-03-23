3e9d589749c7edc51b5eb3ef8cfb56b91c582e11
vulcanism <cynomania@gmail.com>
2020-03-23 12:14:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log("I've got spurs that jingle jangle jingle")} />
   </div>,
   document.getElementById('root')
 );
